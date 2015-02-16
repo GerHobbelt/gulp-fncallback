1503a176708d155727383e4b48f2a40b76e19339
Ger Hobbelt <ger@hobbelt.com>
2015-02-16 19:15:13 +0100

    update/augmentation: the new interface! Now allows you to spec both the regular per-file callback and the 'end/flush' callback, plus pass the options into these callbacks.
    Fundamentally, we now have a gulp plugin around `through2` with a few tweaks.

--- index.js
+++ index.js
@@ -1,45 +1,95 @@
 var through = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
-var defaults = require('lodash.defaults');
 
 const PLUGIN_NAME = 'gulp-callback';
 
-module.exports = function (fn, options) {
-    options = defaults(options || {}, {
-        once: false
-    });
+// options:
+// - `once` (boolean) : invoke the `transformFunction` only once at the start.
+// 
+// Note:
+// 
+// The `callback` function passed to `transformFunction(chunk, encoding, callback)` as an argument
+// extends the regular `through2` interface for this by offering a third argument `doNotCopy` (boolean)
+// which the userland code may use to signal that the default `gulp-callback` action (copying the
+// processed chunk verbatim) is to be skipped.
+module.exports = function (transformFunction, flushFunction, options) {
+    var plugin = this;
+
+    // `transformFunction` is optional
+    if (typeof transformFunction !== 'function' && transformFunction) {
+        throw new PluginError(PLUGIN_NAME, 'transformFunction callback is not a function');
+    }
+    // `flushFunction` is optional
+    if (typeof flushFunction !== 'function' && flushFunction) {
+        if (!options && typeof flushFunction === 'object') {
+            options = flushFunction;
+            flushFunction = null;
+        } else if (options) {
+            throw new PluginError(PLUGIN_NAME, 'flushFunction callback is not a function');
+        }
+    }
+    // yet at least either of the callbacks MUST have been specified
+    if (!transformFunction && !flushFunction) {
+        throw new PluginError(PLUGIN_NAME, 'You have specified neither a valid transformFunction callback function nor a valid flushFunction callback function');
+    }
+    // `options` is optional
+    if (options && typeof options !== 'object') {
+        throw new PluginError(PLUGIN_NAME, 'options is not an options object');
+    }
+
+    options = options || {};
     var once = false;
+    var streamOptions = options.streamOptions = options.streamOptions || {
+        // See for what's inside here: https://github.com/rvagg/through2#api
+        objectMode: true
+    };
 
-    return through.obj(function (file, enc, cb) {
-        var self = this;
+    plugin.options = options;
 
-        var callback = function () {
-            if (options.once) {
-                once = true;
-            }
+    return through(streamOptions, function (file, enc, cb) {
+        // Pass file through if:
+        // - file has no contents
+        // - file is a directory
+        if (file.isNull() || file.isDirectory()) {
+            this.push(file);
+            return cb();
+        }
 
-            if (arguments[0] !== null) {
-                if (arguments[0]) {
-                    self.push(arguments[0]);
+        // we dont do streams (yet)
+        if (file.isStream()) {
+          return this.emit('error', new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+        }
+
+        // This extends the `callback` function interface compared to
+        // https://github.com/rvagg/through2#transformfunction
+        // 
+        // See also the note further above where this gulp plugin's options
+        // are described.
+        var callback = function (err, chunk, doNotCopy) {
+            once = options.once;
+
+            if (!err && !chunk) {
+                if (doNotCopy) {
+                    cb();
                 } else {
-                    self.push(file);
+                    cb(null, file);
                 }
+            } else {
+                cb(err, chunk);
             }
-
-            cb();
         };
 
-        if (typeof(fn) === 'function') {
-            if (!once) {
-                return fn(file, enc, callback);
-            } else {
-                this.push(file);
-
-                cb();
-            }
+        if (!once && transformFunction) {
+            return transformFunction.call(this, file, enc, callback, plugin);
         } else {
-            throw new PluginError(PLUGIN_NAME, 'Callback is not function');
+            cb();
+        }
+    }, function (cb) {
+        if (flushFunction) {
+            return flushFunction.call(this, cb, plugin);
+        } else {
+            cb();
         }
     });
 };
